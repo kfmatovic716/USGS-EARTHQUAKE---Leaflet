60deb51c16e774882d6972d36d0bda56222da1dc
Katherine Matovic <jabuka611@gmail.com>
2021-05-28 22:07:03 -0700

    Modify js file

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,21 +1,41 @@
 // We create the tile layers that will be the selectable backgrounds of our map.
 
 // Create a L.tilelayer() using the 'mapbox/light-v10' map id
-var grayMap = YOUR_CODE_HERE
+var grayMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", 
+{ attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v10",
+  accessToken: API_KEY});
+
 
 // Create a L.tilelayer() using the 'mapbox/satellite-v9' map id
-var satelliteMap = YOUR_CODE_HERE
+var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", 
+{ attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY});
 
-// Create a L.tilelayer() using the 'mapbox/satellite-v9' map id
-var outdoorsMap = YOUR_CODE_HERE
+// Create a L.tilelayer() using the 'mapbox/outdoors-v9' map id
+var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", 
+{ attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/outdoors-v9",
+  accessToken: API_KEY});
 
 
 // We then create the map object with options. Adding the tile layers we just
 // created to an array of layers.
 
+var mapOptions = {center: [20.78, -6],
+  zoom: 3}
 // Create a L.map(), reference the 'mapid' element in the HTML page, and pass in the three layers above
-var myMap = YOUR_CODE_HERE
-
+var myMap = L.map("mapid", mapOptions);
 
 // We create the layers for our two different sets of data, earthquakes and
 // tectonicplates.
@@ -26,27 +46,36 @@ var earthquakes = new L.LayerGroup();
 // of these maps will be visible at a time!
 // Create a basemaps object for the three tileLayers from above. 
 // The key should be a human readable name for the tile layer, and the value should be a tileLayer variable
-var baseMaps = {YOUR_CODE_HERE};
+var baseMaps = {
+  Outdoor: outdoorsMap,
+  Satellite: satelliteMap,
+  Grayscale: grayMap
+};
 
 // We define an object that contains all of our overlays. Any combination of
 // these overlays may be visible at the same time!
 
 // Create a overlays object for the two LayerGroups from above. 
 // The key should be a human readable name for the layer group, and the value should be a LayerGroup variable
-var overlayMaps = {YOUR_CODE_HERE};
+var overlayMaps = {
+  Earthquakes: earthquakes,
+  "Tectonic Plates": tectonicPlates
+};
 
 // Add a L.control.layers() object and pass in the baseMaps and overlayMaps, and then .addTo myMap
-YOUR_CODE_HERE;
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 // Use d3.json() to call the API endpoint for earthquake geoJSON data, 
 // .then() fire off an anonymous function that takes a single argument `data`.
-YOUR_CODE_HERE {
+var earthquakeAllWeek = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(earthquakeAllWeek).then(function(data) {
   // Use L.geoJson() to parse the data, and do the following:
   L.geoJson(data, {
     // use pointToLayer to convert each feature to an L.circleMarker, see https://geospatialresponse.wordpress.com/2015/07/26/leaflet-geojson-pointtolayer/ for a tutorial
     // use style to set the color, radius, and other options for each circleMarker dynamically using the magnitude data
     // use onEachFeature to bind a popup with the magnitude and location of the earthquake to the layer (see above tutorial for an example)
-  }).YOUR_CODE_HERE;  // use .addTo to add the L.geoJson object to the earthquakes LayerGroup
+  }).addTo();  // use .addTo to add the L.geoJson object to the earthquakes LayerGroup
 
   // Then we add the earthquake layer to our map.
   YOUR_CODE_HERE; // use .addTo to add the earthquakes LayerGroup to the myMap object
@@ -58,12 +87,12 @@ YOUR_CODE_HERE {
   // BONUS
   // Make another d3.json() call to the tectonic plates API endpoint
   // then fire off an anonymous function that takes a single argument plateData
-  YOUR_CODE_HERE {
-      // Create an L.geoJson() that reads the plateData, and sets some options per your choosing 
-      YOUR_CODE_HERE
-      .YOUR_CODE_HERE; // use .addTo() to add the l.geoJson layer to the tectonicPlates LayerGroup
+//   YOUR_CODE_HERE {
+//       // Create an L.geoJson() that reads the plateData, and sets some options per your choosing 
+//       YOUR_CODE_HERE
+//       .YOUR_CODE_HERE; // use .addTo() to add the l.geoJson layer to the tectonicPlates LayerGroup
 
-      // Then add the tectonicplates layer to the map.
-      YOUR_CODE_HERE; // use .addTo to add the tectonicPlates LayerGroup to the myMap object
-    });
-});
+//       // Then add the tectonicplates layer to the map.
+//       YOUR_CODE_HERE; // use .addTo to add the tectonicPlates LayerGroup to the myMap object
+//     });
+// });
